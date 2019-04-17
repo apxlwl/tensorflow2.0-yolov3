b8ec49f180329e7381337c07dee18709730c4170
gwl <gwl743416427@gmail.com>
2019-04-17 09:50:44 +0800

    update

--- main_voc_mine.py
+++ main_voc_mine.py
@@ -16,12 +16,12 @@ args = opt.opt
 args.experiment_name = 'voc_scrach'
 args.dataset_name='VOC'
 args.dataset_root='/home/gwl/datasets/VOCdevkit'
-args.learning_rate = 0.0001
+args.learning_rate = 0.00001
 args.config_path = './configs/voc.json'
 args.total_epoch = 300
 args.log_iter = 5000
 # args.resume = 'load_darknet'
-args.resume = 80
+args.resume = 115 
 # args.do_test = True
 with open(args.config_path, 'r') as f:
   configs = json.load(f)
